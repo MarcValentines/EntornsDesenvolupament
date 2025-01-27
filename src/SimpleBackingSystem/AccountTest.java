4179bcf97c0555e3da6b2f851dfb5eb908fd11fe
Carlos <131690161+Yosoyina@users.noreply.github.com>
2025-01-27 19:54:19 +0100

    Cuentas Creadas
    Vemos las cuentas que hemos creado de cada uno del grupo

--- src/SimpleBackingSystem/AccountTest.java
+++ src/SimpleBackingSystem/AccountTest.java
@@ -2,9 +2,14 @@ package SimpleBackingSystem;
 
 public class AccountTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	class AccountTesting{
+		
+		Account myAccount1 = new Account("Carlos Meruvia", "41242154232342", 4500);
+		Account myAccount2 = new Account("Marc Valentines", "5235y734134534", 7500);
+		
+		
+		
+		
 	}
 
 }
