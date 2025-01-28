054925810a0ecfc3c78fe2046980098488f8455c
MarcValentines <marcvalentinesmiro@gmail.com>
2025-01-28 15:40:53 +0100

    AccountTest

---
+++ src/SimpleBackingSystem/AccountTest.java
@@ -0,0 +1,19 @@
+package SimpleBackingSystem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+	@Test
+	void testDepositAmount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testWithdrawAmount() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/module-info.java
+++
@@ -1,3 +0,0 @@
-module DepuracioDeCodi {
-	exports SimpleBackingSystem;
-}
